eceaabbe4eaf860f2998bb811455301ad7fe931a
Grandsummoner <gsnextninja@gmail.com>
2026-03-15 11:25:08 +0800

    Update plugin.hpp

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -1,1 +1,9 @@
+#pragma once
+#include <rack.hpp>
 
+using namespace rack;
+
+extern Plugin* pluginInstance;
+
+// This declares your module so the rest of the app can see it
+extern Model* modelRytmCC;
