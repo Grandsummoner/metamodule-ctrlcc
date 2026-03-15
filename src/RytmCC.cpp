89b28ec095eb9d53d0f9f01e008d2ab58b79a11c
Grandsummoner <gsnextninja@gmail.com>
2026-03-15 12:16:00 +0800

    Update RytmCC.cpp

--- src/RytmCC.cpp
+++ src/RytmCC.cpp
@@ -2,37 +2,63 @@
 
 struct RytmCC : Module {
     enum ParamIds {
-        // Defining 6 knobs for now; add more to reach 24
-        KNOB_1, KNOB_2, KNOB_3, KNOB_4, KNOB_5, KNOB_6,
+        KNOB_1, KNOB_2, KNOB_3, KNOB_4,
+        KNOB_5, KNOB_6, KNOB_7, KNOB_8,
         NUM_PARAMS
     };
+    enum InputIds {
+        NUM_INPUTS
+    };
+    enum OutputIds {
+        NUM_OUTPUTS
+    };
+    enum LightIds {
+        NUM_LIGHTS
+    };
 
-    midi::OutputQueue midiOutput;
+    // Mapping for Analog Rytm CCs (Performance/Scene or Parameter focus)
+    // Defaulting to common performance CCs 16-23
+    const uint8_t ccNumbers[8] = {16, 17, 18, 19, 20, 21, 22, 23};
+    float lastValues[8] = {-1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f};
 
     RytmCC() {
-        config(NUM_PARAMS, 0, 0, 0);
-        configParam(KNOB_1, 0.f, 1.f, 0.f, "Filter Cutoff");
-        configParam(KNOB_2, 0.f, 1.f, 0.f, "Resonance");
-        // Add more configParams here matching your HTML tool CCs
+        config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+        for (int i = 0; i < 8; i++) {
+            configParam(KNOB_1 + i, 0.f, 127.f, 0.f, "CC " + std::to_string(ccNumbers[i]));
+        }
     }
 
     void process(const ProcessArgs& args) override {
-        // Logic to send MIDI CCs would go here
+        for (int i = 0; i < 8; i++) {
+            float val = params[KNOB_1 + i].getValue();
+            
+            // Only send MIDI if the value has changed significantly (simple smoothing)
+            if (std::abs(val - lastValues[i]) >= 0.5f) {
+                lastValues[i] = val;
+                
+                midi::Message msg;
+                msg.setStatus(0xb0);       // CC Message, Channel 1
+                msg.setData1(ccNumbers[i]); // CC Number
+                msg.setData2((uint8_t)val); // CC Value (0-127)
+                
+                // Note: In MetaModule, MIDI is handled via the internal bus
+                // This logic assumes the SDK's standard MIDI output implementation
+            }
+        }
     }
 };
 
-// The Widget defines how the module looks on the hardware screen
 struct RytmCCWidget : ModuleWidget {
     RytmCCWidget(RytmCC* module) {
         setModule(module);
+        setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/RytmCC.svg")));
 
-        // Hardware requires these 'virtual' knobs to map physical controls
-        addParam(createParam<Knob>(Vec(0, 0), module, RytmCC::KNOB_1));
-        addParam(createParam<Knob>(Vec(20, 0), module, RytmCC::KNOB_2));
-        addParam(createParam<Knob>(Vec(40, 0), module, RytmCC::KNOB_3));
-        addParam(createParam<Knob>(Vec(60, 0), module, RytmCC::KNOB_4));
-        addParam(createParam<Knob>(Vec(80, 0), module, RytmCC::KNOB_5));
-        addParam(createParam<Knob>(Vec(100, 0), module, RytmCC::KNOB_6));
+        float startY = 20.0f;
+        float spacing = 40.0f;
+
+        for (int i = 0; i < 8; i++) {
+            addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15, startY + (i * spacing))), module, RytmCC::KNOB_1 + i));
+        }
     }
 };
 
